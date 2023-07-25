5743f3a81eb3f6dd8eda55f9c9b5915c8b91befd
ReyserLyn <rzapata@unsa.edu.pe>
2023-07-25 13:41:40 -0500

    Tutorial corregido

--- Tutorial/urls.py
+++ Tutorial/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework import routers
-from Tutorial.quickstart import views
+from quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
